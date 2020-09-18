8b2f5edeae7ce63024ee3ced225a94a8d64f8e21
jingfreeks <lyndell.dobluis@gmail.com>
2020-09-18 22:25:34 +0800

    Updates

---
+++ precache-manifest.cdb004159a0c1e48bf8894ab35ce2ef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa6e4670d5d20b46430c2e8ca532aa13",
+    "url": "/RobotFlex-Inc/index.html"
+  },
+  {
+    "revision": "b02dc587abefe8f18846",
+    "url": "/RobotFlex-Inc/static/css/main.79505e7c.chunk.css"
+  },
+  {
+    "revision": "665c0bd4e2ac82a0fc08",
+    "url": "/RobotFlex-Inc/static/js/2.e94638ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RobotFlex-Inc/static/js/2.e94638ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b02dc587abefe8f18846",
+    "url": "/RobotFlex-Inc/static/js/main.4b6d5347.chunk.js"
+  },
+  {
+    "revision": "bed1326c7f078b052f1d",
+    "url": "/RobotFlex-Inc/static/js/runtime-main.a7f28e20.js"
+  }
+]);
