11c6e68e54c94d43987707873e70bec1ca36212e
jingfreeks <lyndell.dobluis@gmail.com>
2020-09-18 22:27:12 +0800

    adding new files for github page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import {CardList} from './component/card-list/card-list.component';
+import {SearchBox} from './component/search-box/search-box.component';
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      robots:[],
+      searchField:'',
+    }
+  }
+  componentDidMount=()=>{
+    this.fethData();
+  }
+  fethData=()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(users=>this.setState({robots:users}))
+  }
+  handleChange=(e)=>{
+    this.setState({searchField:e.target.value})
+  }
+  render(){
+    const {robots,searchField}=this.state
+    const filteredRobots = robots.filter(robots=>
+      robots.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    )
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1>Robotex Inc</h1>
+        <SearchBox 
+          placeholder={'search robbots'}
+          handleChange={
+            this.handleChange
+          }
+          value={this.state.searchField} 
+        />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 }
-
 export default App;
